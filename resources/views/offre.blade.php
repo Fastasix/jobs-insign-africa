d8378f81a0befe2ccdc6b32d996e4ee17c339177
Thomas_020303 <97157791+Fastasix@users.noreply.github.com>
2023-02-02 17:58:55 +0000

    ajout modif css et offre blade php

--- resources/views/offre.blade.php
+++ resources/views/offre.blade.php
@@ -3,8 +3,14 @@
 @section('contenu')
 
 <section class="home">
-    <div class="">
-        {{$offreid->titre}}
+    <div class="position-container-offre">
+        <div class="title-container-offer">
+            <div class="title-position-offer">Descriptif de l'offre : {{$offreid->titre}}</div>
+            <div class="offer-reference"> Référence de l'offre : {{$offreid->reference_offre}}</div>
+        </div>
+        <div class="">
+            <div class=""></div>
+        </div>
     </div>
 </section>
 
@@ -14,6 +20,6 @@
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = "{{asset('assets/css/candidature.css')}}";
+    link.href = "{{asset('assets/css/offre.css')}}";
     document.getElementsByTagName('HEAD')[0].appendChild(link);
 </script>
